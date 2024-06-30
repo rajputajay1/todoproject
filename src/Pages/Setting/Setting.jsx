a67841a317b6814e47d4ce000b34f03f3cffd21b
ajayrajuput <120632665+ajayrajuput@users.noreply.github.com>
2024-06-30 14:55:35 +0530

    issue solve

--- src/Pages/Setting/Setting.jsx
+++ src/Pages/Setting/Setting.jsx
@@ -1,40 +1,81 @@
-import React from 'react'
-import "./Setting.css"
+import React from "react";
+import "./Setting.css";
+import { useState } from "react";
 
 const Setting = () => {
-    return (
-        <>
-            <div className='settingcontainer'>
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [oldPasswordVisible, setOldPasswordVisible] = useState(false);
 
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
 
-                <p className='setting'>Setting</p>
-                <div>
-                    <div className="input-container">
-                        <img src="./Profile.svg" alt="" className='icon-class' />
-                        <input type="text" placeholder='Name' />
-                    </div>
-                    <div className="input-container">
-                        <img src="./mails.svg" alt="" className='icon-class 'id='mailsimg' />
-                        <input type="text" placeholder='Update Email' />
-                    </div>
-                    <div className="input-container">
-                        <img src="./pass.svg" alt="" className='icon-class' />
-                        <input type="text" placeholder='Old Password' />
-                        <img src="./seen.svg" alt="" className='icon-class-end' />
-                    </div>
-                    <div className="input-container">
-                        <img src="./pass.svg" alt="" className='icon-class' />
-                        <input type="text" placeholder='New Password' />
-                        <img src="./seen.svg" alt="" className='icon-class-end' />
+  const toggleConfirmPasswordVisibility = () => {
+    setOldPasswordVisible(!oldPasswordVisible);
+  };
 
-                    </div>
-                    <div className='updatebtn'>
-                        <p className='updatetext'>Update</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  const passwordStrength = (password) => {
+    if (password.length < 4) {
+      return "Weak";
+    } else if (password.length < 6) {
+      return "Medium";
+    } else {
+      return "Strong";
+    }
+  };
+  return (
+    <>
+      <div className="settingcontainer">
+        <p className="setting">Setting</p>
+        <div>
+          <div className="input-container">
+            <img src="./Profile.svg" alt="" className="icon-class" />
+            <input type="text" placeholder="Name" />
+          </div>
+          <div className="input-container">
+            <img
+              src="./mails.svg"
+              alt=""
+              className="icon-class "
+              id="mailsimg"
+            />
+            <input type="text" placeholder="Update Email" />
+          </div>
+          <div className="input-container">
+            <img src="./pass.svg" alt="" className="icon-class" />
+            <input
+              type={passwordVisible ? "text" : "password"}
+              placeholder="Old Password"
+            />
+            <img
+              src="./seen.svg"
+              alt=""
+              className="icon-class-end"
+              onClick={togglePasswordVisibility}
+            />
+          </div>
+          <div className="input-container">
+            <img src="./pass.svg" alt="" className="icon-class" />
+            <input
+              type={oldPasswordVisible ? "text" : "password"}
+              placeholder="New Password"
+            />
+            <img
+              src="./seen.svg"
+              alt=""
+              className="icon-class-end"
+              onClick={toggleConfirmPasswordVisibility}
+            />
 
-export default Setting
+            <p className="password-strength">{passwordStrength}</p>
+          </div>
+          <div className="updatebtn">
+            <p className="updatetext">Update</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Setting;
